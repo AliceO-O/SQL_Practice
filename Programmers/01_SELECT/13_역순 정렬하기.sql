bb5930ac49cab7947e1d29a764081580372b8c92
AliceO-O <128025084+AliceO-O@users.noreply.github.com>
2026-09-21 01:05:01 +0900

    26.09.02

---
+++ Programmers/01_SELECT/13_역순 정렬하기.sql
@@ -0,0 +1,14 @@
+/* 
+[ANIMAL_INS] 동물 보호소에 들어온 동물의 정보 테이블
+
+ANIMAL_ID 동물의 아이디
+ANIMAL_TYPE 생물 종
+DATETIME 보호 시작일
+INTAKE_CONDITION 보호 시작 시 상태
+NAME 이름
+SEX_UPON_INTAKE 성별 및 중성화 여부
+ */
+
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
